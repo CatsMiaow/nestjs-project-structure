2a25e18fdd11d9143d3a0edba05def832d3e9437
CatsMiaow <infiltrator@naver.com>
2019-12-24 16:57:04 +0900

    Add config object deep merge

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,8 +1,24 @@
 import { config } from './default';
 
+export const util = {
+  isObject<T>(value: T): boolean {
+    return value !== null && typeof value === 'object' && Array.isArray(value) === false;
+  },
+  // tslint:disable-next-line: no-any
+  merge(target: Record<string, any>, source: Record<string, any>) {
+    for (const key of Object.keys(source)) {
+      if (this.isObject(target[key]) && this.isObject(source[key])) {
+        Object.assign(source[key], this.merge(target[key], source[key]));
+      }
+    }
+
+    return { ...target, ...source };
+  }
+};
+
 export const configuration = async () => {
   const environment = await import(`./${process.env.NODE_ENV || 'development'}`);
 
-  // or deep merge
-  return { ...config, ...environment.config };
+  // object deep merge
+  return util.merge(config, environment.config);
 };
