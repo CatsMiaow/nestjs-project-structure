b7527cf2f7cf7a598c518235678596dc574a714e
CatsMiaow <infiltrator@naver.com>
2020-03-09 14:34:38 +0900

    Add prompts to entity generator
    Update package.json

--- bin/entity.js
+++ bin/entity.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 const fs = require('fs');
 const path = require('path');
+const prompts = require('prompts');
 const rimraf = require('rimraf');
 const shell = require('shelljs');
 
@@ -9,54 +10,66 @@ if (!process.env.DB_HOST) {
   throw new Error('Create a .env file');
 }
 
-/**
- * npm run entity dbname [skip]
- */
 process.env.PATH += (path.delimiter + path.join(process.cwd(), 'node_modules', '.bin'));
-if (process.argv.length < 3) {
-  throw new Error('RequiredDatabase');
-};
+(async () => {
+try {
+  const response = await prompts([{
+    type: 'text',
+    name: 'db',
+    message: 'Please enter a database name.',
+    validate: value => !!value
+  }, /* {
+    type: 'select',
+    name: 'db',
+    message: 'Please select a database name.',
+    choices: [
+      { value: 'db1' },
+      { value: 'db2' }
+    ],
+  }, */ {
+    type: 'confirm',
+    name: 'build',
+    message: 'Choose whether to proceed with the build.',
+    initial: true
+  }]);
 
-const db = process.argv[2];
-const isBuild = process.argv[3] !== 'skip';
+  const { db, isBuild } = response;
+  const MODEL_DIR = path.join(__dirname, '../src/entity', db);
+  rimraf.sync(`${MODEL_DIR}/*`);
 
-/* const DATABASE = [];
-if (!DATABASE.includes(db)) {
-  throw new Error('InvalidDatabase');
-} */
+  const generatorConfig = [
+    '--noConfig',
+    '--cf none', // file names
+    '--ce pascal', // class names
+    '--cp none', // property names
+    `--namingStrategy ${path.join(__dirname, 'NamingStrategy.js')}`,
+    `-h ${process.env.DB_HOST}`,
+    `-p ${process.env.DB_PORT}`,
+    // https://github.com/Kononnable/typeorm-model-generator/issues/204#issuecomment-533709527
+    // If you use multiple databases, add comma.
+    `-d ${db}`, // `-d ${db},`,
+    `-u ${process.env.DB_USER}`,
+    `-x ${process.env.DB_PASSWORD}`,
+    `-e ${process.env.DB_TYPE}`,
+    `-o ${MODEL_DIR}`
+  ];
+  shell.exec(`typeorm-model-generator ${generatorConfig.join(' ')}`);
 
-const MODEL_DIR = path.join(__dirname, '../src/entity', db);
-rimraf.sync(`${MODEL_DIR}/*`);
+  const files = [];
+  fs.readdirSync(MODEL_DIR).forEach((file) => {
+    files.push(`export * from './${file.replace('.ts', '')}';`);
+  });
+  files.push('');
+  // export entity db tables
+  // AS-IS import { Tablename } from './entity/dbname/tablename';
+  // TO-BE import { Tablename } from './entity/dbname';
+  fs.writeFileSync(path.join(MODEL_DIR, 'index.ts'), files.join('\n'));
 
-const generatorConfig = [
-  '--noConfig',
-  '--cf none', // file names
-  '--ce pascal', // class names
-  '--cp none', // property names
-  `--namingStrategy ${path.join(__dirname, 'NamingStrategy.js')}`,
-  `-h ${process.env.DB_HOST}`,
-  `-p ${process.env.DB_PORT}`,
-  // https://github.com/Kononnable/typeorm-model-generator/issues/204#issuecomment-533709527
-  // If you use multiple databases, add comma.
-  `-d ${db}`, // `-d ${db},`,
-  `-u ${process.env.DB_USER}`,
-  `-x ${process.env.DB_PASSWORD}`,
-  `-e ${process.env.DB_TYPE}`,
-  `-o ${MODEL_DIR}`
-];
-shell.exec(`typeorm-model-generator ${generatorConfig.join(' ')}`);
-
-const files = [];
-fs.readdirSync(MODEL_DIR).forEach((file) => {
-  files.push(`export * from './${file.replace('.ts', '')}';`);
-});
-files.push('');
-// export entity db tables
-// AS-IS import { Tablename } from './entity/dbname/tablename';
-// TO-BE import { Tablename } from './entity/dbname';
-fs.writeFileSync(path.join(MODEL_DIR, 'index.ts'), files.join('\n'));
-
-// Build skips and builds manually when errors occur
-if (isBuild) {
-  shell.exec('npm run build:entity');
+  // Build skips and builds manually when errors occur
+  if (isBuild) {
+    shell.exec('npm run build:entity');
+  }
+} catch (error) {
+  throw new Error(error);
 }
+})();
