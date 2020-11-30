b0d528e6121e1687786d0e948cac8049e5812f96
CatsMiaow <infiltrator@naver.com>
2020-11-30 15:17:50 +0900

    Update packages

--- jest.config.js
+++ jest.config.js
@@ -60,7 +60,7 @@ module.exports = {
   // A set of global variables that need to be available in all test environments
   globals: {
     "ts-jest": {
-      "tsConfig": "tsconfig.json"
+      "tsconfig": "tsconfig.json"
     }
   },
 
