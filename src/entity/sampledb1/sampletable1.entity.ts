178e7ac2099eede005df715f0dbdfc9a2f9cc8cf
CatsMiaow <infiltrator@naver.com>
2021-01-12 18:19:21 +0900

    Refactor sample entity

--- src/entity/sampledb1/sampletable1.entity.ts
+++ src/entity/sampledb1/sampletable1.entity.ts
@@ -1,41 +1,22 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity('sampletable1', { schema: 'sampledb1', database: 'sampledb1' })
+@Entity('sampletable1')
 export class Sampletable1 {
-  @PrimaryGeneratedColumn({
-    type: 'int',
-    unsigned: true,
-    name: 'id',
-  })
+  @PrimaryGeneratedColumn({ type: 'int', unsigned: true, name: 'id' })
   id!: number;
 
-  @Column('varchar', {
-    nullable: false,
-    length: 255,
-    name: 'title',
-  })
+  @Column('varchar', { nullable: false, length: 255, name: 'title' })
   title!: string;
 
-  @Column('text', {
-    nullable: true,
-    name: 'content',
-  })
+  @Column('text', { nullable: true, name: 'content' })
   content?: string;
 
   @Column('simple-array')
   tags?: string[];
 
-  @Column('timestamp', {
-    nullable: false,
-    default: () => 'CURRENT_TIMESTAMP',
-    name: 'updated_at',
-  })
+  @Column('timestamp', { nullable: false, default: () => 'CURRENT_TIMESTAMP', name: 'updated_at' })
   updated_at!: Date;
 
-  @Column('timestamp', {
-    nullable: false,
-    default: () => 'CURRENT_TIMESTAMP',
-    name: 'created_at',
-  })
+  @Column('timestamp', { nullable: false, default: () => 'CURRENT_TIMESTAMP', name: 'created_at' })
   created_at!: Date;
 }
--- src/entity/sampledb2/sampletable2.entity.ts
+++ src/entity/sampledb2/sampletable2.entity.ts
@@ -1,38 +1,19 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity('sampletable2', { schema: 'sampledb2', database: 'sampledb2' })
+@Entity('sampletable2')
 export class Sampletable2 {
-  @PrimaryGeneratedColumn({
-    type: 'int',
-    unsigned: true,
-    name: 'id',
-  })
+  @PrimaryGeneratedColumn({ type: 'int', unsigned: true, name: 'id' })
   id!: number;
 
-  @Column('varchar', {
-    nullable: false,
-    length: 255,
-    name: 'title',
-  })
+  @Column('varchar', { nullable: false, length: 255, name: 'title' })
   title!: string;
 
-  @Column('text', {
-    nullable: true,
-    name: 'content',
-  })
+  @Column('text', { nullable: true, name: 'content' })
   content?: string;
 
-  @Column('timestamp', {
-    nullable: false,
-    default: () => 'CURRENT_TIMESTAMP',
-    name: 'updated_at',
-  })
+  @Column('timestamp', { nullable: false, default: () => 'CURRENT_TIMESTAMP', name: 'updated_at' })
   updated_at!: Date;
 
-  @Column('timestamp', {
-    nullable: false,
-    default: () => 'CURRENT_TIMESTAMP',
-    name: 'created_at',
-  })
+  @Column('timestamp', { nullable: false, default: () => 'CURRENT_TIMESTAMP', name: 'created_at' })
   created_at!: Date;
 }
