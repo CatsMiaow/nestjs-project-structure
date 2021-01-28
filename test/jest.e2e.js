b3560530182ac31dddad0fd71bfbc95388c74995
CatsMiaow <infiltrator@naver.com>
2021-01-28 17:24:02 +0900

    Refactor e2e testing for gql plugin

--- test/jest.e2e.js
+++ test/jest.e2e.js
@@ -4,7 +4,18 @@ const config = require('../jest.config');
 // https://github.com/nestjs/graphql/issues/810#issuecomment-618308354
 module.exports = {
   ...config,
-  rootDir: '../dist',
-  testMatch: ['**/e2e/**/*.+(spec|test).js'],
+  rootDir: '.',
+  testMatch: ['**/e2e/**/*.+(spec|test).[tj]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.e2e.json',
+      // https://kulshekhar.github.io/ts-jest/docs/options/astTransformers
+      astTransformers: {
+        before: ['<rootDir>/jest.e2e.transformer.js'],
+      },
+      // https://kulshekhar.github.io/ts-jest/docs/options/isolatedModules
+      isolatedModules: true,
+    },
+  },
 };
---
+++ test/jest.e2e.transformer.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+const transformer = require('@nestjs/graphql/plugin');
+
+// https://docs.nestjs.com/graphql/cli-plugin#integration-with-ts-jest-e2e-tests
+module.exports = {
+  name: 'nestjs-graphql-transformer',
+  version: 1,
+  factory: (cs) => transformer.before({}, cs.tsCompiler.program)
+};
