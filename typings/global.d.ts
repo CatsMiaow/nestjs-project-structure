5e672d08e5a984aa1e9e59f35bf7faf50262b278
CatsMiaow <infiltrator@naver.com>
2021-08-31 13:23:25 +0900

    Add rules to compilerOptions

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -1,6 +1,22 @@
 import { Payload } from '../src/auth';
 
 export declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: string;
+      PORT: string;
+
+      DB_TYPE: string;
+      DB_HOST: string;
+      DB_PORT: string;
+      DB_USER: string;
+      DB_PASSWORD: string;
+      DB_NAME: string;
+
+      JWT_SECRET: string;
+    }
+  }
+
   namespace Express {
     interface Request {
       id: string;
