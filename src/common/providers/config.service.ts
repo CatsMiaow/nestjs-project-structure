3facc9d9e9c399d548028848265114df6abddbcf
CatsMiaow <infiltrator@naver.com>
2021-11-26 16:00:23 +0900

    Fix config path type

--- src/common/providers/config.service.ts
+++ src/common/providers/config.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService as NestConfig, Path, PathValue } from '@nestjs/config';
+import { ConfigService as NestConfig, Path as NestPath } from '@nestjs/config';
 
-import type { Config } from '../../config';
+import type { Config, Path, PathValue } from '../../config';
 
 @Injectable()
 export class ConfigService<K = Config> extends NestConfig<K> {
   public override get<P extends Path<K>>(path: P): PathValue<K, P> {
-    const value = super.get(path, { infer: true });
+    const value = super.get(<NestPath<K>><unknown>path, { infer: true });
 
     if (value === undefined) {
       throw new Error(`NotFoundConfig: ${path}`);
     }
 
-    return value;
+    return <PathValue<K, P>>value;
   }
 }
--- src/config/config.interface.ts
+++ src/config/config.interface.ts
@@ -1,3 +1,5 @@
+import type { PathImpl2 } from '@nestjs/config';
+
 import type { config as base } from './envs/default';
 import type { config as production } from './envs/production';
 
@@ -5,3 +7,7 @@ export type Objectype = Record<string, unknown>;
 export type Default = typeof base;
 export type Production = typeof production;
 export type Config = Default & Production;
+
+// https://github.com/nestjs/config/issues/752
+export type Path<T> = PathImpl2<T> extends string | keyof T ? PathImpl2<T> : keyof T;
+export type PathValue<T, P extends Path<T>> = P extends `${infer Key}.${infer Rest}` ? Key extends keyof T ? Rest extends Path<T[Key]> ? PathValue<T[Key], Rest> : never : never : P extends keyof T ? T[P] : never;
