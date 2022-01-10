4d122296b47ca88d924e2017dd0deb36de15fc12
CatsMiaow <infiltrator@naver.com>
2022-01-10 15:10:54 +0900

    Fix non-async function in DebugLog

--- src/debug/debug-log.decorator.ts
+++ src/debug/debug-log.decorator.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import { Logger } from '@nestjs/common';
 import { performance } from 'perf_hooks';
+// import { isAsyncFunction } from 'util/types'; // >= v15.3.0
+import { types } from 'util';
 
 const MethodLog = (context?: string): MethodDecorator => (
   (target: Object, propertyKey: string | symbol, descriptor: PropertyDescriptor): void => {
@@ -9,20 +11,34 @@ const MethodLog = (context?: string): MethodDecorator => (
       return;
     }
 
-    descriptor.value = async function (...args: unknown[]): Promise<unknown> {
-      const start = performance.now();
-      const result: unknown = await originalMethod.apply(this, args);
-      const end = performance.now();
-
-      const time = (end - start).toFixed(2);
+    const log = function (time: number, args: unknown[]): void {
       const ownKey = (typeof target === 'function') ? `${target.name}` : '';
       const name = context ? `${ownKey}.${String(propertyKey)}` : String(propertyKey);
       const params = (args.length > 0) ? `(${args})` : '';
 
-      Logger.debug(`${name}${params} +${time}ms`, context || ownKey);
-      // or Use result to add response log
-      return result;
+      Logger.debug(`${name}${params} +${time.toFixed(2)}ms`, context || ownKey);
     };
+
+    if (types.isAsyncFunction(originalMethod)) {
+      descriptor.value = async function (...args: unknown[]): Promise<unknown> {
+        const start = performance.now();
+        const result: unknown = await originalMethod.apply(this, args);
+        const end = performance.now();
+
+        log(end - start, args);
+        // or Use result to add response log
+        return result;
+      };
+    } else {
+      descriptor.value = function (...args: unknown[]): unknown {
+        const start = performance.now();
+        const result: unknown = originalMethod.apply(this, args);
+        const end = performance.now();
+
+        log(end - start, args);
+        return result;
+      };
+    }
   }
 );
 
@@ -45,7 +61,7 @@ const ClassLog = (context?: string): ClassDecorator => (
       if (originalMethod !== descriptor.value) {
         const metadataKeys = Reflect.getMetadataKeys(originalMethod);
         for (const key of metadataKeys) {
-          const value = Reflect.getMetadata(key, originalMethod);
+          const value: unknown = Reflect.getMetadata(key, originalMethod);
           Reflect.defineMetadata(key, value, <Object>descriptor.value);
         }
       }
