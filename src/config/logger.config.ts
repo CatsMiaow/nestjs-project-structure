925444923f2029d933588dfb4e68d4530947b916
CatsMiaow <infiltrator@naver.com>
2022-07-27 14:02:06 +0900

    Fix logger timestamp

--- src/config/logger.config.ts
+++ src/config/logger.config.ts
@@ -5,10 +5,13 @@ import type { Params } from 'nestjs-pino';
 import { multistream } from 'pino';
 import type { ReqId } from 'pino-http';
 
-const passUrl = ['/health', '/graphql'];
+const passUrl = new Set(['/health', '/graphql']);
 
 export const loggerOptions: Params = {
   pinoHttp: [{
+    // https://getpino.io/#/docs/api?id=timestamp-boolean-function
+    // Change time value in production log.
+    // timestamp: stdTimeFunctions.isoTime,
     quietReqLogger: true,
     genReqId: (req: IncomingMessage): ReqId => (<Request>req).header('X-Request-Id') || nanoid(),
     ...(process.env.NODE_ENV === 'production'
@@ -16,10 +19,13 @@ export const loggerOptions: Params = {
       : {
         level: 'debug',
         // https://github.com/pinojs/pino-pretty
-        transport: { target: 'pino-pretty', options: { singleLine: true } },
+        transport: {
+          target: 'pino-pretty',
+          options: { singleLine: true, translateTime: 'SYS:yyyy-mm-dd HH:MM:ss.l' },
+        },
       }),
     autoLogging: {
-      ignore: (req: IncomingMessage) => passUrl.includes((<Request>req).originalUrl),
+      ignore: (req: IncomingMessage) => passUrl.has((<Request>req).originalUrl),
     },
   }, multistream([
     // https://getpino.io/#/docs/help?id=log-to-different-streams
