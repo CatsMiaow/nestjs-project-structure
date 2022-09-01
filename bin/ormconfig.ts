71c56fad7caad47dc41bff34bd8a4827fa5bfa27
CatsMiaow <infiltrator@naver.com>
2022-09-01 13:54:44 +0900

    Remove hack due to typeorm update

--- bin/ormconfig.ts
+++ bin/ormconfig.ts
@@ -1,26 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /// <reference types="../typings/global" />
 import * as dotenv from 'dotenv';
-import { DataSource, DataSourceOptions, InstanceChecker } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 
 import { configuration } from '../src/config';
 
 dotenv.config();
-
-//#region typeorm hack
-// https://github.com/typeorm/typeorm/issues/8914#issuecomment-1152603148
-function patchAsyncDataSourceSetup(): void {
-  const oldIsDataSource = InstanceChecker.isDataSource;
-  InstanceChecker.isDataSource = function (obj: unknown): obj is DataSource {
-    if (obj instanceof Promise) {
-      return true;
-    }
-    return oldIsDataSource(obj);
-  };
-}
-patchAsyncDataSourceSetup();
-//#endregion
-
 const ormconfig = async (): Promise<DataSource> => {
   const config = <{ db: DataSourceOptions }> await configuration();
 
